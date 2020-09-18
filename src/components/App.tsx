5c13f641d17f67f51bfca60e5ddc0c5b9bc1f855
Leila <nb.leila10@gmail.com>
2020-09-18 17:04:40 -0500

    Connect App to the store and display todos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Todo, fetchTodos } from "../actions/index";
+import { StoreState } from "../reducers/index";
 
 const App = (): JSX.Element => {
-  return <div>Hi There!!</div>;
+  const dispatch = useDispatch();
+
+  const todos: Todo[] = useSelector<StoreState, Todo[]>((state) => state.todos);
+
+  React.useEffect(() => {
+    fetchTodos()(dispatch);
+  }, [dispatch]);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
